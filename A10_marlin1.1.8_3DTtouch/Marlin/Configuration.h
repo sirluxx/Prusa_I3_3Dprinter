00e50ff7129cba4215f12284f77b8c199a8da9e3
sirluxx <39352937+sirluxx@users.noreply.github.com>
2019-10-20 14:01:58 +0200

    Change PROBE_OFFSET for new printer head

--- A10_marlin1.1.8_3DTtouch/Marlin/Configuration.h
+++ A10_marlin1.1.8_3DTtouch/Marlin/Configuration.h
@@ -700,8 +700,8 @@
  *      O-- FRONT --+
  *    (0,0)
  */
-#define X_PROBE_OFFSET_FROM_EXTRUDER -37  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER -0  // Y offset: -front +behind [the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER 36  // X offset: -left  +right  [of the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER 19  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -0.50 // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
